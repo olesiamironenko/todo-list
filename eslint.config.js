29ec7cdef2f7fbb5fe5699b242d17ff9fcfaf397
Olesia <136620028+olesiamironenko@users.noreply.github.com>
2025-07-11 18:37:43 -0400

    Revert "Week 01 setup"

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist", "build"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    extends: [js.configs.recommended],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
-    },
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs["recommended"].rules,
-      ...react.configs["jsx-runtime"].rules,
-      // Optional: Customize a few common rules
-      "no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
-      "react/prop-types": "off", //this suppresses warnings about not using prop-types
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-]);
+])
