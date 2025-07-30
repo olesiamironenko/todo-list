7e65e67f7f37f5020630cac1217b1cc3a3f50e2a
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-29 21:50:33 -0400

    checkbox added to todo

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import TodoListItem from './TodoListItem.jsx';
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onCompleteTodo }) {
   return (
     <ul>
       {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          onCompleteTodo={onCompleteTodo}
+        />
       ))}
     </ul>
   );
