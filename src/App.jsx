78110cff917ea5d15c3259ce76a5201b0aa7affa
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-07-29 21:50:33 -0400

    checkbox added to todo

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,25 @@ import { useState } from 'react';
 function App() {
   // Set statement
   const [todoList, setTodoList] = useState([]);
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
 
   function addTodo(title) {
-    const newTodo = { title, id: Date.now() };
+    const newTodo = {
+      title,
+      id: Date.now(),
+      isCompleted: false,
+    };
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    const completedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(completedTodos);
   }
 
   return (
@@ -17,7 +32,7 @@ function App() {
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
       {todoList.length > 0 ? (
-        <TodoList todoList={todoList} />
+        <TodoList todoList={filteredTodoList} onCompleteTodo={completeTodo} />
       ) : (
         <p>Add todo above to get started</p>
       )}
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,5 +1,16 @@
-function TodoListItem({ todo: { title } }) {
-  return <li>{title}</li>;
+function TodoListItem({ todo, onCompleteTodo }) {
+  return (
+    <li>
+      <form>
+        <input
+          type="checkbox"
+          checked={todo.isCompleted}
+          onChange={() => onCompleteTodo(todo.id)}
+        />
+        {todo.title}
+      </form>
+    </li>
+  );
 }
 
 export default TodoListItem;
