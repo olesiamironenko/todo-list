b064a00aa57b4979428d8af3bd0c700ec46dafbe
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-08-13 07:33:32 -0400

    TextInputWithLabel added to TodoListItem

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
 function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <li>
       <form>
-        <input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={() => onCompleteTodo(todo.id)}
-        />
-        {todo.title}
+        {isEditing ? (
+          <TextInputWithLabel value={todo.title} />
+        ) : (
+          <>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              id={`checkbox${todo.id}`}
+              onChange={() => onCompleteTodo(todo.id)}
+            />
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
       </form>
     </li>
   );
