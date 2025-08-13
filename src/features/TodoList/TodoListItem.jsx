e350ca9b9dd09f1a030c579fde2380a35353f315
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-08-13 08:02:40 -0400

    handleCancel helper and Cancel button added to TodoListItem

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,12 +3,23 @@ import TextInputWithLabel from '../../shared/TextInputWithLabel';
 
 function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleCancel() {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  }
 
   return (
     <li>
       <form>
         {isEditing ? (
-          <TextInputWithLabel value={todo.title} />
+          <>
+            <TextInputWithLabel value={todo.title} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
         ) : (
           <>
             <input
